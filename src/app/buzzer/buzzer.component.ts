a78501f6f28843c457461c45d8a3a4b6cb6ff4fc
Daniel Cook <dmc3152@yahoo.com>
2024-12-03 22:16:07 -1000

    add sounds to buzzer

--- src/app/buzzer/buzzer.component.ts
+++ src/app/buzzer/buzzer.component.ts
@@ -6,11 +6,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { catchError, debounceTime, distinctUntilChanged, EMPTY, firstValueFrom, map, Observable, shareReplay, Subscription, take } from 'rxjs';
-import { BuzzerAvailabilityGQL, BuzzerAvailabilitySubscription, GetUnauthenticatedSelfGQL, SignInAsUnauthenticatedUserGQL, SignInAsUnauthenticatedUserMutationVariables, SubscriptionGQL } from '../../../graphql/generated';
+import { BuzzerAvailabilityGQL, BuzzerAvailabilitySubscription, GetUnauthenticatedSelfGQL, SignInAsUnauthenticatedUserGQL, SignInAsUnauthenticatedUserMutationVariables } from '../../../graphql/generated';
+import { Howl, Howler } from 'howler';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-buzzer',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
   providers: [Document],
   templateUrl: './buzzer.component.html',
   styleUrl: './buzzer.component.scss',
@@ -23,6 +25,19 @@ export class BuzzerComponent implements OnInit {
   private buzzerAvailabilityQuery = inject(BuzzerAvailabilityGQL);
 
   private _buzzerSubscription?: Subscription;
+  private _soundLibrary = [
+    'sounds/bell.mp3',
+    'sounds/carol-of-the-bells.mp3',
+    'sounds/choir.mp3',
+    'sounds/christmas-logo.mp3',
+    'sounds/holiday-song.mp3',
+    'sounds/loop-song.mp3',
+    'sounds/pop.mp3',
+    'sounds/twinkle-charm.mp3',
+    'sounds/whoosh.mp3',
+  ];
+  private _usedSounds: string[] = [];
+  private _sound?: Howl;
 
   constructor() {
     effect(async () => {
@@ -68,10 +83,38 @@ export class BuzzerComponent implements OnInit {
         this.formGroup.controls.name.patchValue(result.data.unauthenticatedSelf.name);
       }
     });
+    this.loadSound();
   }
 
   ngOnDestroy() {
     this._closeBuzzerSubscription();
+  }
+
+  loadSound = () => {
+    if (this._sound) {
+      this._sound.unload();
+    }
+
+    const file = this._getRandomSound([...this._soundLibrary]);
+    this._sound = new Howl({
+      src: [file],
+      loop: true,
+    });
+  }
+
+  private _getRandomSound = (sounds: string[]): string => {
+    if (this._usedSounds.length === this._soundLibrary.length) {
+      this._usedSounds = [];
+    }
+
+    const randomSrcIndex = Math.floor(Math.random() * sounds.length);
+    if (this._usedSounds.includes(sounds[randomSrcIndex])) {
+      sounds.splice(randomSrcIndex, 1);
+      return this._getRandomSound(sounds);
+    }
+
+    this._usedSounds.push(sounds[randomSrcIndex]);
+    return sounds[randomSrcIndex];
   }
 
   private _closeBuzzerSubscription() {
@@ -92,7 +135,6 @@ export class BuzzerComponent implements OnInit {
     name: new FormControl('', Validators.required),
     code: new FormControl('', Validators.required)
   })
-  buttonState = 'inactive';
 
   formIsInvalid = toSignal(this.formGroup.statusChanges.pipe(
     map(status => status === 'INVALID'),
@@ -120,12 +162,10 @@ export class BuzzerComponent implements OnInit {
   })
 
   pressButton() {
-    console.log('Button pressed!');
-    this.buttonState = 'active';
+    this._sound?.play();
   }
 
   releaseButton() {
-    console.log('Button released!');
-    this.buttonState = 'inactive';
+    this._sound?.stop();
   }
 }
